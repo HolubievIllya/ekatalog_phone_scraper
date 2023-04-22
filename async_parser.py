e3d203543a8f12f94bddc4e7ad5cd652849f0fcc
HolubievIllya <detrix548@gmail.com>
2023-04-22 11:05:32 +0300

    IMPROVED

--- async_parser.py
+++ async_parser.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-from datetime import date
+from datetime import datetime
 import asyncio
 import aiofiles
 from aiocsv import AsyncWriter
@@ -9,7 +9,8 @@ from aiocsv import AsyncWriter
 
 async def parse_data():
     ua = UserAgent()
-    url = "https://ek.ua/ua/list/122/samsung/"
+    # url can be https://ek.ua/ua/list/122/apple/
+    url = "YOUR LINK"
     headers = {
         "User-Agent" : ua.random,
         "accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
@@ -18,9 +19,9 @@ async def parse_data():
         "holder" : "1"
         }
 
-    today = date.today()
+    now = datetime.now()
 
-    async with aiofiles.open(f"{today.strftime('%d-%m-%Y')}.csv", "w", encoding="cp1251") as ex_file:
+    async with aiofiles.open(f"{now.strftime('%d-%m-%Y %H-%M-%S')}.csv", "w", encoding="cp1251") as ex_file:
         writer = AsyncWriter(ex_file)
         await writer.writerow(["Товар", "Ціна"])
         while True:
@@ -32,7 +33,6 @@ async def parse_data():
                 await writer.writerow([i.find("a")["title"], j.find("a").text])
             if soup.select_one("a.select + a.ib"):
                 url = "https://ek.ua" + soup.select_one("a.select + a.ib")["href"]
-                print(url)
             else:
                 break
 async def main():
--- parser.py
+++ parser.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-from datetime import date
+from datetime import datetime
 import csv
 
 def parse_data():
     ua = UserAgent()
-    url = "https://ek.ua/ua/list/122/samsung/"
+    # url can be https://ek.ua/ua/list/122/samsung/
+    url = "YOUR LINK"
     headers = {
         "User-Agent" : ua.random,
         "accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
@@ -15,9 +16,9 @@ def parse_data():
         "holder" : "1"
         }
 
-    today = date.today()
+    now = datetime.now()
 
-    with open(f"{today.strftime('%d-%m-%Y')}.csv", "w", encoding="cp1251") as ex_file:
+    with open(f"{now.strftime('%d-%m-%Y %H-%M-%S')}.csv", "w", encoding="cp1251") as ex_file:
         writer = csv.writer(ex_file)
         writer.writerow(["Товар", "Ціна"])
         while True:
@@ -29,7 +30,6 @@ def parse_data():
                 writer.writerow([i.find("a")["title"], j.find("a").text])
             if soup.select_one("a.select + a.ib"):
                 url = "https://ek.ua" + soup.select_one("a.select + a.ib")["href"]
-                print(url)
             else:
                 break
 def main():
